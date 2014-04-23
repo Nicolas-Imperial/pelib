3c36eb755278620bc0f6895ec8398629d705aff8
johja659 <johja659@f5bcb9b3-529a-41a5-a8ab-580fc49d8fe7>
2014-04-23 08:01:40 +0000

    initial stub for taskgraph parser
    git-svn-id: svn://svn.jpunkt.se/exjobb/trunk/code/cppelib@170 f5bcb9b3-529a-41a5-a8ab-580fc49d8fe7

---
+++ include/Taskgraph.hpp
@@ -0,0 +1,19 @@
+#include "RecordParser.hpp"
+#include "RecordOutput.hpp"
+#ifndef PELIB_TASKGRAPH
+#define PELIB_TASKGRAPH
+namespace pelib
+{
+  class Taskgraph: public RecordParser, public RecordOutput
+  {
+  
+
+  };
+}
+
+
+
+
+
+
+#endif
