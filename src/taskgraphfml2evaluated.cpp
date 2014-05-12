b5a2de48657b6b2c8da5e429b6da37517f1f4c59
Johan <johja659@student.liu.se>
2014-05-12 20:34:54 +0200

    removed debug cout output

--- src/taskgraphfml2evaluated.cpp
+++ src/taskgraphfml2evaluated.cpp
@@ -13,43 +13,45 @@ int main(int argc, char **argv)
       cerr << "usage: cat takgraph.graphml | " << argv[0] << " <p8.txt>";
       return 0;
     }
-  cout << "Ok, starting ...\n";
+  //cout << "Ok, starting ...\n";
   AmplInput archparser;
   ifstream ifs(argv[1]);
 
   Record arch = archparser.parse(ifs);
 
-  cout << "this is the arch\n";
+  //cout << "this is the arch\n";
   archparser.dump(cout,arch);
     
 
   Taskgraph taskgparser;
 
-  cout << "parsing cin to taskgraph\n";
+  //cout << "parsing cin to taskgraph\n";
   TaskgraphRecord taskgraph = taskgparser.parse(cin);
 
-  cout << "Setting architecture\n";
+  //cout << "Setting architecture\n";
   taskgraph.setArchitecture(arch);
 
-  cout << "This is the taskgraph:\n";
+  //cout << "This is the taskgraph:\n";
   taskgparser.dump(cout, taskgraph);
 
-  cout << "converting from taskgraph to record\n";
+  //cout << "converting from taskgraph to record\n";
   Record record = taskgraph.toRecord();
 
-  cout << "the record: \n";
-  cout << std::setprecision(6)                                                                         	      << std::setiosflags(std::ios::fixed)                                                            	      << std::setiosflags(std::ios::showpoint);
+  //cout << "the record: \n";
+  cout << std::setprecision(6) 
+       << std::setiosflags(std::ios::fixed)                                                         
+       << std::setiosflags(std::ios::showpoint);
 
   archparser.dump(cout,record);
 
 
 
 
-  cout << "going back to taskgraph\n";
+  //cout << "going back to taskgraph\n";
   TaskgraphRecord tgr(taskgraph,record);
-  cout << "dumping\n";
+  //cout << "dumping\n";
   taskgparser.dump(cout, tgr);
-  cout << "Done\n";
+  //cout << "Done\n";
   
   return 0;
 }
