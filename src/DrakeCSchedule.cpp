af6d452dde691b46f98064c030e0bc14461f92fd
Nicolas Melot <nicolas.melot@liu.se>
2015-07-13 10:26:01 -0700

    * Added convenient debugging enabling/disabling switch

--- src/DrakeCSchedule.cpp
+++ src/DrakeCSchedule.cpp
@@ -21,7 +21,11 @@ extern "C"{
 #include <pelib/CastException.hpp>
 #include <pelib/ParseException.hpp>
 
+#if 0
 #define debug(expr) cout << "[" << __FILE__ << ":" << __FUNCTION__ << ":" << __LINE__ << "] " << #expr << " = \"" << expr << "\"." << endl;
+#else
+#define debug(var)
+#endif
 
 using namespace pelib;
 using namespace std;
